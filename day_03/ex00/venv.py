294a92c0e85fe4a21acea040681cc1535819b059
Greg <retgunner@gmail.com>
2022-09-05 22:51:49 +0300

    ex03

--- day_03/ex00/venv.py
+++ day_03/ex00/venv.py
@@ -2,5 +2,8 @@
 
 import os
 
+def run():
+    print(os.environ["VIRTUAL_ENV"])
 
-print(os.environ["VIRTUAL_ENV"])
+if __name__ == "__main__":
+    run()
--- day_03/ex02/librarian.py
+++ day_03/ex02/librarian.py
@@ -1,14 +1,18 @@
 import os
 import subprocess
 
+def run():
+    try:
+        assert os.environ["VIRTUAL_ENV"].endswith("ex02/bteak")
+        subprocess.call(["pip", "install", "beautifulsoup4", "pytest"])
+        freeze = subprocess.check_output(["pip", "freeze"]).decode("utf-8")
+        print(freeze, end="")
+        with open("requirements.txt", "w") as file:
+            file.write(freeze)
 
-try:
-    assert os.environ["VIRTUAL_ENV"].endswith("ex02/bteak")
-    subprocess.call(["pip", "install", "beautifulsoup4", "pytest"])
-    freeze = subprocess.check_output(["pip", "freeze"]).decode("utf-8")
-    print(freeze, end="")
-    with open("requirements.txt", "w") as file:
-        file.write(freeze)
+    except Exception as e:
+        print(f"Error caught! {e}")
 
-except Exception as e:
-    print(f"Error caught! {e}")
+
+if __name__ == "__main__":
+    run()
