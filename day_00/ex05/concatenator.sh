7d245290adfc18390aa2d3585d1017328304e011
Greg <retgunner@gmail.com>
2022-09-17 13:37:47 +0300

    ex05

--- day_00/ex05/concatenator.sh
+++ day_00/ex05/concatenator.sh
@@ -1,1 +1,7 @@
 #!/bin/sh
+awk -F, '{if(NR==1) {header = $0; print $0 > "hh_concat.csv"; next}}
+{if ($0 != header) print $0 >> "hh_concat.csv"}
+' ${@}
+cat hh_concat.csv| head -n 1 > hh_concat_sorted.csv
+cat hh_concat.csv | tail -n +2 | sort -k2,2 -k1,1 -t, >> hh_concat_sorted.csv
+rm hh_concat.csv
